69cea815f3e275bb0b76f630375cd907ed42230c
Erick Rios <paulrios@outlook.es>
2023-06-02 00:38:33 -0500

    Initialize frontend repository with basic configuration

---
+++ src/main.ts
@@ -0,0 +1,20 @@
+
+import { createApp } from 'vue'
+import PrimeVue from 'primevue/config'
+import { createPinia } from 'pinia'
+//theme
+import "primevue/resources/themes/lara-light-indigo/theme.css";
+//core
+import "primevue/resources/primevue.min.css";
+//icons
+import 'primeicons/primeicons.css';
+
+import App from './App.vue'
+import router from './router'
+
+const app = createApp(App);
+
+app.use(createPinia());
+app.use(router);
+app.use(PrimeVue);
+app.mount('#app');
