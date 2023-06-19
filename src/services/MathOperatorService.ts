4f8fdf7a4094ddb9fe1be60ad8dcc4a3752269c2
Erick Rios <paulrios@outlook.es>
2023-06-19 12:33:49 -0500

    feat: Implement API versioning functionality

--- src/services/MathOperatorService.ts
+++ src/services/MathOperatorService.ts
@@ -2,7 +2,7 @@ import { useApiPrivate } from '@/composables/useApi'
 
 export const getMathResult = async (payload: any) => {
   try {
-    const { data } = await useApiPrivate().post(`/api/operation/math-operation`, payload)
+    const { data } = await useApiPrivate().post(`/operation/math-operation`, payload)
     return data
   } catch (error: Error | any) {
     throw error.response.data
--- src/services/RandomStringService.ts
+++ src/services/RandomStringService.ts
@@ -2,7 +2,7 @@ import { useApiPrivate } from '@/composables/useApi'
 
 export const getRandomString = async (payload: any) => {
   try {
-    const { data } = await useApiPrivate().post(`/api/operation/random-string`, payload)
+    const { data } = await useApiPrivate().post(`/operation/random-string`, payload)
     return data
   } catch (error: Error | any) {
     throw error.message
